baecae975aacd7ed9d945501c6afc4a9444c5e34
TheEnquirer <huxmarv@nuevaschool.org>
2021-07-08 16:17:18 -0700

    see function + constats

--- SFMLSlime/SFMLSlime/main.cpp
+++ SFMLSlime/SFMLSlime/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cmath>
 
+const int WIDTH = 256 * 2;
+const int HEIGHT = 160 * 2;
+int canvas[WIDTH][HEIGHT] = {0};
 
 struct Agent
 {
@@ -17,9 +20,35 @@ float lerp(float a, float b, float f)
     return a + f * (b - a);
 }
 
-const int WIDTH = 256;
-const int HEIGHT = 160;
-int canvas[WIDTH][HEIGHT] = {0};
+float see(Agent agent, float sensorSpacingOffset)
+{
+    int sensorSize = 10;
+    float sensorAngle = agent.angle + sensorSpacingOffset;
+    float sensorDirX = sin(sensorAngle);
+    float sensorDirY = cos(sensorAngle);
+    int sensorCenterX = round(agent.x + sensorDirX * 2);
+    int sensorCenterY = round(agent.y + sensorDirY * 2);
+    float ssum = 0;
+
+    for (int ox = -sensorSize; ox <= sensorSize; ox++)
+    {
+	for (int oy = -sensorSize; oy <= sensorSize; oy++)
+	{
+	    int posX = sensorCenterX + ox;
+	    int posY = sensorCenterY + oy;
+
+	    if (posX >= 0 && posX < WIDTH && posY >= 0 && posY < HEIGHT)
+	    {
+		ssum += canvas[posX][posY];
+	    }
+	}
+    }
+
+    //std::cout << sum << std::endl;
+    //std::cout<<"test";
+    return ssum;
+}
+
 
 int main()
 {
@@ -39,14 +68,14 @@ int main()
     r1.setSize(sf::Vector2f(3, 3));
     r1.setPosition(0, 0);
 
-    std::vector<Agent> agents(200);
+    std::vector<Agent> agents(1800);
 
 
     int i = 0;
     for (auto& agent : agents)
     {
-	agent.x = rand() % 100;
-	agent.y = rand() % 100;
+	agent.x = rand() % WIDTH;
+	agent.y = rand() % HEIGHT;
 	//agent.x = i+20;
 	//agent.y = i+20;
 	agent.angle = rand() % 7;
@@ -82,10 +111,34 @@ int main()
 
 	for (auto& agent : agents) {
 	    // modify angle with sensor
+	    float sensorSpacing = 2;
+	    float turnSpeed = 0.5;
 
+	    float wf = see(agent, 0);
+	    float wl = see(agent, sensorSpacing);
+	    float wr = see(agent, -sensorSpacing);
 
+	    float stear = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	    //std::cout << wf << ":" << wl << ":" << wr << std::endl;
 
+	    if (wf > wl && wf > wr)
+	    {
+		agent.angle += 0;
 
+	    } else if (wf < wl && wf < wr)
+	    {
+		agent.angle += (stear - 0.5) * 2 * turnSpeed;
+		//std::cout << "hii";
+
+	    } else if (wr > wl)
+	    {
+		agent.angle -= stear * turnSpeed;
+	    } else if (wl > wr)
+	    {
+		agent.angle += stear * turnSpeed;
+	    }
+
+	    //agent.angle += stear * 5;
 
 
 
@@ -132,7 +185,7 @@ int main()
 	    {
 		if (canvas[x][y] != 0)
 		{
-		    int reduce = canvas[x][y] * 1;
+		    int reduce = canvas[x][y] * 0.97;
 		    if (reduce < 0)
 		    {
 			canvas[x][y] = 0;
@@ -171,8 +224,8 @@ int main()
 		float blur = sum / 9;
 
 		float zero = 0;
-		float diffused = lerp(canvas[x][y], blur, 1.02);
-		float max_sec = diffused - 0.001;
+		float diffused = lerp(canvas[x][y], blur, 0.8);
+		float max_sec = diffused - 0.01;
 		float evaporated = std::max(zero, max_sec);
 		//std::cout << sum << blur << diffused << std::endl;
 		canvas[x][y] = evaporated;
