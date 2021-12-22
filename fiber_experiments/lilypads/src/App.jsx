64e36971df1920086bfe58e4e0db42916532ee76
Huxley Marvit <huxmarv@nuevaschool.org>
2021-12-22 02:11:39 +0000

    some epic cubes

--- fiber_experiments/lilypads/src/App.jsx
+++ fiber_experiments/lilypads/src/App.jsx
@@ -1,21 +1,18 @@
-import ReactDOM from 'react-dom'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import Box from './comps/Box.jsx'
 
-function App() {
+export default function App() {
     return (
-	<div id="canvas-container">
-	    whee
+	<div className="w-screen h-screen border-0 border-red-400">
 	    <Canvas>
-		<mesh>
-		    <sphereGeometry
-		    />
-		    <meshStandardMaterial />
-		</mesh>
-		<ambientLight intensity={0.1} />
-		<directionalLight color="red" position={[0, 0, 5]} />
+		<ambientLight intensity={0.5} />
+		<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+		<pointLight position={[-10, -10, -10]} />
+		<Box position={[-1.2, 0, 0]} />
+		<Box position={[1.2, 0, 0]} />
 	    </Canvas>
 	</div>
     )
 }
 
-export default App;
