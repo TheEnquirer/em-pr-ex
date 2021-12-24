c67715772c8ab23eb51d6dd37ca5e57486902844
Huxley Marvit <huxmarv@nuevaschool.org>
2021-12-24 05:23:53 +0000

    NW: movement w/ lilypads

--- fiber_experiments/lilypads/src/comps/Lily.jsx
+++ fiber_experiments/lilypads/src/comps/Lily.jsx
@@ -3,18 +3,61 @@ import React, { useRef, useState, useEffect, Suspense} from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import SimplexNoise from 'simplex-noise';
 
 function Lily(props) {
-
-    //const gltf = useLoader(GLTFLoader, '../models/all_lilys.glb')
+    const [objs, setObjs] = useState([])
+    const [t, setT] = useState(0)
+    const simplex = new SimplexNoise()
     const gltf = useLoader(GLTFLoader, '/word_lilys2.gltf')
 
-    //for (let i = 0; i < gltf.scene.children.length; i++) {
-    //    gltf.scene.children[i].onClick= () => console.log("ive been clicked!", i)
+    useEffect(() => {
+	let tobjs = gltf.scene.children
+	tobjs = tobjs.map((e, i) => {
+	    return <primitive 
+		object={gltf.scene.children[i]} 
+		//position={[0, 0.1, 0]}
+		position={[e.position.x, e.position.y + 0.1, e.position.z]}
+		onClick={() => console.log("clicked!", i)}
+	    />
+	})
+	setObjs(tobjs)
+    }, []);
+    //value3d = simplex.noise3D()
+    //console.log(value3d)
 
-    //}
+    //const gltf = useLoader(GLTFLoader, '../models/all_lilys.glb')
 
-    //useFrame((state, delta) => {
+
+    useFrame((state, delta) => {
+	//setT(t+0.01)
+	//setT(t+0.1)
+	setObjs(objs.map((e, i) => {
+	    //let val = simplex.noise3D(e.props.object.position.x, e.props.object.position.z, e.props.object.position.y)
+	    let val = Math.sin(t) / 500
+	    //let val = Math.sin(t) / 100
+	    if (Math.sin(t) < 0.1 && Math.sin(t) > -0.1) {
+	    //if (Math.sin(t) > 0.9) {
+		if (!e.props.object.flip) {
+		    e.props.object.direction = (Math.random() - 0.5) * 2
+		    console.log("resetting direction")
+		}
+		e.props.object.flip = true;
+	    }
+	    else { e.props.object.flip = false; }
+
+	    //let slope = -0.5
+	//    //console.log(val, e.props.position)
+	//    //console.log(e.props.position)
+	//    //e.props.position[2] += 0.1
+
+	    e.props.object.position.z += val
+	    e.props.object.position.x += e.props.object.direction * val
+
+	//    //console.log(e.props.position)
+	    return e
+	}))
+    })
     //    for (let i = 1; i < gltf.scene.children.length; i++) {
     //        gltf.scene.children[i].position.x += (Math.random() -0.5) / 100
     //        //gltf.scene.children[0].position.x = gltf.scene.children[1].position.x 
@@ -23,22 +66,11 @@ function Lily(props) {
     //    //gltf.scene.children[2].position.x = 10
     //    //console.log(gltf.scene.children[0].position.x)
     //})
-    let objs = gltf.scene.children
-    objs = objs.map((e, i) => {
-	return <primitive 
-	    object={gltf.scene.children[i]} 
-	    //position={[0, 0.1, 0]}
-	    position={[e.position.x, e.position.y + 0.1, e.position.z]}
-	    onClick={() => console.log("clicked!", i)}
-	/>
-	//return 1
-    })
-    console.log(objs)
-
-    //console.log(gltf.scene.children)
+    
     return (
-	<>
-	    {objs}
+	<> 
+      	    {objs} 
+	    {/*{console.log(objs[0])}*/}
 	</>
     )
 }
